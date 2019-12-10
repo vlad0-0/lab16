5755b4abc4da631f4bfce88cf6b76399f8a04dad
vlad0-0 <54848408+vlad0-0@users.noreply.github.com>
2019-12-10 13:51:20 +0300

    Update 16.2.py

--- 16.2.py
+++ 16.2.py
@@ -1,13 +1,18 @@
 n = int(input())
 a = []
+b = []
 c = 0
 for i in range(n):
     a.append(int(input()))
-for i in range(n):
+i = 0
+while len(a)>1:
     if a.count(a[i]) == 2:
         c = a[i]
-        del(a[a.index(a[i])])
-        del(a[a.index(a[i])])
-        n -= 2
-print(n)
+        del(a[a.index(c)])
+        del(a[a.index(c)])
+    elif i < len(a)-1:
+        i += 1
+    else:
+        break
+print(len(a))
 print(a)
