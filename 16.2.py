e0b3b67a5c4bf968340fd20e1eb530a12503cc98
vlad0-0 <54848408+vlad0-0@users.noreply.github.com>
2019-12-10 12:51:41 +0300

    Update 16.2.py

--- 16.2.py
+++ 16.2.py
@@ -1,11 +1,13 @@
 n = int(input())
 a = []
-b = []
-c = []
+c = 0
 for i in range(n):
     a.append(int(input()))
-b[:] = a[:]
-while i < n:
-    for j in range
-print(len(a))
+for i in range(n):
+    if a.count(a[i]) == 2:
+        c = a[i]
+        del(a[a.index(a[i])])
+        del(a[a.index(a[i])])
+        n -= 2
+print(n)
 print(a)
